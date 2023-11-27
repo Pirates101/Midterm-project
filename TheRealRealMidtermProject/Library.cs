71c5ddfdd55c67263e4130bdc8a9ce14180a5a8b
Frank Wedlick <jfwedlick@gmail.com>
2023-11-27 18:43:33 -0500

    search by title and author

--- TheRealRealMidtermProject/Library.cs
+++ TheRealRealMidtermProject/Library.cs
@@ -17,18 +17,18 @@ namespace TheRealRealMidtermProject
 
             LibraryBooks = new List<Book>();
 
-            Book book1 = new Book("Stephen King", "The Shining", false, null);
-            Book book2 = new Book("Robert Jordan", "The Eye of the World", true, null);
-            Book book3 = new Book("Stieg Larsson", "The Girl With the Dragon Tattoo", false, null);
-            Book book4 = new Book("Rebecca Yaros", "Fourth Wing", false, null);
-            Book book5 = new Book("Sara Novic", "True Biz", false, null);
-            Book book6 = new Book("Bonnie Garmus", "Lessons in Chemistry", false, null);
-            Book book7 = new Book("Dan Abnett", "Fell Cargo", false, null);
-            Book book8 = new Book("Sarah Penner", "The Lost Apothecary", false, null);
-            Book book9 = new Book("Stacy Willingham", "A Flicker in the Dark", true, null);
-            Book book10 = new Book("Suzanne Collins", "The Ballad of Songbirds and Snakes", false, null);
-            Book book11 = new Book("Gabrielle Zevin", "Tomorrow and Tomorrow and Tomorrow", false, null);
-            Book book12 = new Book("William King", "Dragonslayer", false, null);
+            Book book1 = new Book(1, "Stephen King", "The Shining", false, null);
+            Book book2 = new Book(2, "Robert Jordan", "The Eye of the World", true, null);
+            Book book3 = new Book(3, "Stieg Larsson", "The Girl With the Dragon Tattoo", false, null);
+            Book book4 = new Book(4, "Rebecca Yaros", "Fourth Wing", false, null);
+            Book book5 = new Book(5, "Sara Novic", "True Biz", false, null);
+            Book book6 = new Book(6, "Bonnie Garmus", "Lessons in Chemistry", false, null);
+            Book book7 = new Book(7, "Dan Abnett", "Fell Cargo", false, null);
+            Book book8 = new Book(8, "Sarah Penner", "The Lost Apothecary", false, null);
+            Book book9 = new Book(9, "Stacy Willingham", "A Flicker in the Dark", true, null);
+            Book book10 = new Book(10, "Suzanne Collins", "The Ballad of Songbirds and Snakes", false, null);
+            Book book11 = new Book(11, "Gabrielle Zevin", "Tomorrow and Tomorrow and Tomorrow", false, null);
+            Book book12 = new Book(12, "William King", "Dragonslayer", false, null);
 
             LibraryBooks.Add(book1);
             LibraryBooks.Add(book2);
@@ -56,22 +56,35 @@ namespace TheRealRealMidtermProject
 
         public void SearchByAuthor()
         {
-            Console.WriteLine("Enter the author you want to search by ");
+            List<Book> searchResults = new List<Book>();
+            Console.Write("Enter the author you want to search by: ");
             string userAuthor = Console.ReadLine().Trim();
+            Console.WriteLine();
+            int idx = 1;
             foreach (Book book in LibraryBooks)
             {
                 if (book.Author.Contains(userAuthor, StringComparison.OrdinalIgnoreCase))
                 {
                     if (book.CheckedOut == false)
                     {
-                        Console.WriteLine($"{book.Title} is available");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"{idx}. (Book ID: {book.BookID}) {book.Title} by {book.Author} - Status: AVAILABLE");
+                        searchResults.Add(book);
+                        idx++;
                     }
                     else
                     {
-                        Console.WriteLine($"{book.Title} is checked out");
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"{idx}. (Book ID: {book.BookID}) {book.Title} by {book.Author} - Status: CHECKEDOUT");
+                        idx++;
                     }
                 }
             }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine();
+            Console.Write($"If you would like to check out one of these books, enter the Book ID here or enter 0 to go back to the main menu: ");
+            var userInput = Console.ReadLine();
+            int.TryParse(userInput, out int bookSelection);
         }
 
         public void SearchByTitle()
@@ -88,21 +101,21 @@ namespace TheRealRealMidtermProject
                     if (book.CheckedOut == false)
                     {
                         Console.ForegroundColor= ConsoleColor.Green;
-                        Console.WriteLine($"{idx}. {book.Title} by {book.Author} - Status: AVAILABLE");
+                        Console.WriteLine($"{idx}. (Book ID: {book.BookID}) {book.Title} by {book.Author} - Status: AVAILABLE");
                         searchResults.Add(book);
                         idx++;
                     }
                     else
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"{idx}. {book.Title} by {book.Author} - Status: CHECKEDOUT");
+                        Console.WriteLine($"{idx}. (Book ID: {book.BookID}) {book.Title} by {book.Author} - Status: CHECKEDOUT");
                         idx++;
                     }
                 }
             }
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine();
-            Console.WriteLine($"If you would like to check out one of these books, input the number below.\nInput 0 to go back to the main menu.");
+            Console.Write($"If you would like to check out one of these books, enter the Book ID here or enter 0 to go back to the main menu: ");
             var userInput = Console.ReadLine();
             int.TryParse(userInput, out int bookSelection);
         }
