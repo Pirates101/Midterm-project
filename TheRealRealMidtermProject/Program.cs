4954a566ca245e957eb93f7646bf49d27c3f09b3
Aimee Rzepka <rzepka.aimee@gmail.com>
2023-11-27 19:23:40 -0500

    aimee

--- TheRealRealMidtermProject/Program.cs
+++ TheRealRealMidtermProject/Program.cs
@@ -6,9 +6,14 @@
         {
             Library library = new Library();
 
+            Console.WriteLine("Welcome to the Poutine Tots Libary. \nSee below for a list of our books. You can also search by title or author to check availability.");
+            Console.WriteLine();
+
             library.DisplayBooks();
 
             //library.SearchByAuthor();
+            Console.WriteLine();
+
             library.SearchByTitle();
         }
     }
