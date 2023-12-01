53edac9fb909e9cdaa0a9521a7aa277d98a341e0
Benjamin Holmes <manbej.sholem@gmail.com>
2023-12-01 18:27:58 -0500

    Burn down

---
+++ TheRealRealMidtermProject/AsciiArt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheRealRealMidtermProject
+{
+    internal sealed record AsciiArt(
+        string Art,
+        int Width,
+        int Height);
+
+
+}
--- TheRealRealMidtermProject/Program.cs
+++ TheRealRealMidtermProject/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System.Net.Mail;
 
+
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
 namespace TheRealRealMidtermProject
 {
     public class Program
@@ -56,6 +61,13 @@ namespace TheRealRealMidtermProject
                     break;
                 }
 
+                else if (userResponse == "6")
+                {
+                    Console.Clear();
+                    BurnItDown();
+                    break;
+                }
+
                 else
                 {
                     Console.WriteLine($"Invalid selection. Please choose from the list below.");
@@ -106,6 +118,29 @@ namespace TheRealRealMidtermProject
                     book.CheckedOut = false;
                 }
             }
+        }
+
+        static void BurnItDown()
+        {
+            string imagePath = @"C:\Users\manbe\dev\TheRealRealMidtermProject\Fire2.jpg";
+
+            using var inputStream = new FileStream(
+                imagePath,
+                FileMode.Open, 
+                FileAccess.Read,
+                FileShare.Read);
+
+            var generator = new Generator();
+
+            using var sourceImage = Image.Load(inputStream);
+
+            using var imageRgba32 = sourceImage.CloneAs<Rgba32>();
+
+            using var image = new ImageSharpImageSource(imageRgba32);
+
+            var asciiArt = generator.GenerateAsciiArtFromImage(image);
+
+            Console.WriteLine(asciiArt.Art);
         }
 
     }
